e7a08356499daebdfaad60cd303f4254dfc79d84
pycuber <134294337+pete4radio@users.noreply.github.com>
2025-12-10 20:13:51 -0800

    one minor change (SMesh.info)

--- src/graphics/draw/UIRenderer.cpp
+++ src/graphics/draw/UIRenderer.cpp
@@ -940,7 +940,7 @@ void UIRenderer::drawIconScreen(const char *upperMsg, OLEDDisplay *display, OLED
 
     display->setFont(FONT_MEDIUM);
     display->setTextAlignment(TEXT_ALIGN_LEFT);
-    const char *title = "meshtastic.org";
+    const char *title = "SMesh.info";
     display->drawString(x + getStringCenteredX(title), y + SCREEN_HEIGHT - FONT_HEIGHT_MEDIUM, title);
     display->setFont(FONT_SMALL);
     // Draw region in upper left
